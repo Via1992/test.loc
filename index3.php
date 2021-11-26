bed5493b76f04fda5b2b851fe9e639d518d67880
Violetta Polyakova <vapolyakova@avito.ru>
2021-11-26 14:43:45 +0300

    Homework 4

---
+++ index3.php
@@ -0,0 +1,14 @@
+<?php
+
+$dirs = scandir('./images');
+$path = './images/';
+$images = [];
+foreach ($dirs as $dir) {
+    if ($dir !== '.' && $dir !== '..') {
+        $images[] = $dir;
+    }
+}
+
+foreach ($images as $image) {
+    echo '<a target="_blank" href="' . $path . $image . '"><img src="' . $path . $image . '" style = width:200px></a>';
+}
